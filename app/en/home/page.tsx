88edde56ce83f8fe0f1654fbf0ecaee5a79073bb
infotest563-pixel <infotest563@gmail.com>
2026-04-14 18:29:47 +0530

    .

--- app/en/home/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function EnglishHomePage() {
-    // Redirect /en/home to /
-    redirect('/');
-}
--- app/sv/home/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function SwedishHomePage() {
-    // Redirect /sv/home to /sv
-    redirect('/sv');
-}
