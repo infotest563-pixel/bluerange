75a9926e452a199130db9640079615105ae43561
infotest563-pixel <infotest563@gmail.com>
2026-04-14 10:40:06 +0530

    .

--- components/Header.tsx
+++ components/Header.tsx
@@ -110,6 +110,9 @@ export default async function Header() {
                         <DeskToggle />
 
                     </div>
+                    <li>
+    <LanguageSwitcher />
+</li>
                 </nav>
             </header>
         </div>
